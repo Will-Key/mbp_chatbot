62afc25b4619771886ed0163cf2b1a9ffe540ca1
Will-Key <saibe.akreiry@gmail.com>
2024-07-21 11:32:52 +0000

    Generate flow resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FlowModule } from './flow/flow.module';
 
 @Module({
-  imports: [],
+  imports: [FlowModule],
   controllers: [AppController],
   providers: [AppService],
 })

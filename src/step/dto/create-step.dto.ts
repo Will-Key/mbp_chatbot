a7dc4d6d1e9e5cbef94d8fa280235a40311d399d
Will-Key <saibe.akreiry@gmail.com>
2024-07-22 22:02:54 +0000

    Implement CRUD functionnality for step module

--- src/step/dto/create-step.dto.ts
+++ src/step/dto/create-step.dto.ts
@@ -1,1 +1,23 @@
-export class CreateStepDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+
+export class CreateStepDto {
+  @IsNotEmpty()
+  @IsNumber()
+  level: number
+
+  @IsNotEmpty()
+  @IsString()
+  message: string
+
+  @IsNotEmpty()
+  @IsString()
+  expectedResponse: string
+
+  @IsNotEmpty()
+  @IsString()
+  expectedResponseLength: number
+
+  @IsNotEmpty()
+  @IsNumber()
+  flowId: number
+}
--- src/step/step.service.ts
+++ src/step/step.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStepDto } from './dto/create-step.dto';
-import { UpdateStepDto } from './dto/update-step.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateStepDto } from './dto/create-step.dto'
+import { UpdateStepDto } from './dto/update-step.dto'
+import { PrismaService } from '../../prisma/prisma.service'
 
 @Injectable()
 export class StepService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createStepDto: CreateStepDto) {
-    return 'This action adds a new step';
+    return this.prismaService.step.create({ data: createStepDto })
   }
 
   findAll() {
-    return `This action returns all step`;
+    return this.prismaService.step.findMany()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} step`;
+    return this.prismaService.step.findUnique({ where: { id } })
   }
 
-  update(id: number, updateStepDto: UpdateStepDto) {
-    return `This action updates a #${id} step`;
+  update(id: number, data: UpdateStepDto) {
+    return this.prismaService.step.update({
+      data,
+      where: { id },
+    })
   }
 
   remove(id: number) {
-    return `This action removes a #${id} step`;
+    return this.prismaService.step.delete({ where: { id } })
   }
 }

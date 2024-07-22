35ced805b83e0e11df11381ee9eb6d85934bbf0e
Will-Key <saibe.akreiry@gmail.com>
2024-07-22 22:03:59 +0000

    Create Conversation resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlowModule } from './flow/flow.module';
 import { StepModule } from './step/step.module';
+import { ConversationModule } from './conversation/conversation.module';
 
 @Module({
-  imports: [FlowModule, StepModule],
+  imports: [FlowModule, StepModule, ConversationModule],
   controllers: [AppController],
   providers: [AppService],
 })

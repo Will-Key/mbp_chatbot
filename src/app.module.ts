b683a9d3bcc3a98c5512e71395ca408095599a7d
Will-Key <saibe.akreiry@gmail.com>
2024-07-22 21:46:21 +0000

    Create Step resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlowModule } from './flow/flow.module';
+import { StepModule } from './step/step.module';
 
 @Module({
-  imports: [FlowModule],
+  imports: [FlowModule, StepModule],
   controllers: [AppController],
   providers: [AppService],
 })

68dd46d2a9dd09cd73d0319879d7c46c5df99a5c
Will-Key <60333393+Will-Key@users.noreply.github.com>
2024-08-12 19:36:49 +0000

    Import prisma into history-conversation.module.ts

--- src/history-conversation/history-conversation.module.ts
+++ src/history-conversation/history-conversation.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HistoryConversationService } from './history-conversation.service';
 import { HistoryConversationController } from './history-conversation.controller';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [HistoryConversationController],
-  providers: [HistoryConversationService],
+  providers: [HistoryConversationService, PrismaService],
 })
-export class HistoryConversationModule {}
+export class HistoryConversationModule { }

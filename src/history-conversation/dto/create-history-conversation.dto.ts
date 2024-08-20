2ceff4c3b935562a939fea26824e55a69655ce3e
Will-Key <60333393+Will-Key@users.noreply.github.com>
2024-08-20 12:09:35 +0000

    Implement base method of history conversation

---
+++ src/history-conversation/dto/create-history-conversation.dto.ts
@@ -0,0 +1,11 @@
+import { HistoryConversationReasonForEnding, HistoryConversationStatus } from "@prisma/client"
+
+export class CreateHistoryConversationDto {
+    whaPhoneNumber: string
+
+    stepId?: number
+
+    status: HistoryConversationStatus
+
+    reason: HistoryConversationReasonForEnding
+}
--- src/history-conversation/history-conversation.service.ts
+++ src/history-conversation/history-conversation.service.ts
@@ -1,11 +1,39 @@
+
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import { CreateHistoryConversationDto } from './dto/create-history-conversation.dto';
+import { UpdateHistoryConversationDto } from './dto/update-history-conversation.dto';
 
 @Injectable()
 export class HistoryConversationService {
     constructor(private readonly prismaService: PrismaService) { }
 
-    create() { }
+    create(payload: CreateHistoryConversationDto) {
+        return this.prismaService.historyConversation.create({
+            data: payload
+        })
+    }
 
+    update(id: number, payload: UpdateHistoryConversationDto) {
+        return this.prismaService.historyConversation.update({
+            where: {
+                id
+            },
+            data: payload
+        })
+    }
 
+    findOneByWhaPhoneNumber(whaPhoneNumber: string) {
+        return this.prismaService.historyConversation.findFirst({
+            where: { whaPhoneNumber },
+            orderBy: { createdAt: 'desc' }
+        })
+    }
+
+    findOneByWhaPhoneNumberAndFlowId(whaPhoneNumber: string, stepId: number) {
+        return this.prismaService.historyConversation.findFirst({
+            where: { whaPhoneNumber, stepId },
+            orderBy: { createdAt: 'desc' }
+        })
+    }
 }

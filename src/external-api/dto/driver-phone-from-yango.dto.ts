9b4126889ef73b297be34b3fa3fdd4183379d548
Will-Key <saibe.akreiry@gmail.com>
2025-07-27 16:20:08 +0000

    Create Object for updating driver phone number

--- src/external-api/dto/driver-phone-from-yango.dto.ts
+++ src/external-api/dto/driver-phone-from-yango.dto.ts
@@ -4,6 +4,41 @@ class Account {
   work_rule_id: string
 }
 
+class OrderProvider {
+  partner: boolean
+  platform: boolean
+}
+
+class ContactInfo {
+  phone: string
+}
+
+class DriverLicense {
+  country: string
+  expiry_date: string
+  issue_date: string
+  number: string
+}
+
+class FullName {
+  first_name: string
+  last_name: string
+}
+
+class Person {
+  contact_info: ContactInfo
+  driver_license: DriverLicense
+  full_name: FullName
+}
+
+class Profile {
+  hire_date: string
+  work_status: 'working' | 'not_working' | 'fired'
+}
+
 export class DriverPhoneFromYango {
   account: Account
+  order_provider: OrderProvider
+  person: Person
+  profile: Profile
 }
